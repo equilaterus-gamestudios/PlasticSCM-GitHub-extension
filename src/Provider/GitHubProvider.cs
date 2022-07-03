f051131a47993633fffad6407422ea396ad6315e
dacanizares <dacanizares@outlook.com>
2022-07-03 02:04:11 -0500

    Updated API calls supporting linux

--- src/Provider/GitHubProvider.cs
+++ src/Provider/GitHubProvider.cs
@@ -15,12 +15,17 @@ namespace Equilaterus.GitHubExtension.Provider
 		static readonly ILog _log = LogManager.GetLogger("githubextension");
 		private readonly IssueTrackerConfiguration _configuration;
 		private readonly IGitHubHttpHelper _httpHelper;
-
+		private int _timeout;
+		private bool _linux;
 
 		public GitHubProvider(IssueTrackerConfiguration configuration, IGitHubHttpHelper httpHelper)
 		{
 			_configuration = configuration;
 			_httpHelper = httpHelper;
+
+			// Set global config
+			_timeout = configuration.GetTimeout(_log);
+			_linux = configuration.GetLinux(_log);
 		}
 
 		public PlasticTask FindSingleTaskById(string taskId)
@@ -33,7 +38,7 @@ namespace Equilaterus.GitHubExtension.Provider
 
 			// Call API
 			var url = _configuration.GetApiUrlForSingleTask(taskId);
-			if (!_httpHelper.TryCallApi(url, _configuration.GetAuthToken(), out var apiResponse))
+			if (!_httpHelper.TryCallApi(url, _configuration.GetAuthToken(), out var apiResponse, _timeout, _linux))
 			{
 				return null;
 			}
@@ -54,7 +59,7 @@ namespace Equilaterus.GitHubExtension.Provider
 		{
 			// Call API
 			var url = _configuration.GetApiUrlForTasks(assignee);	
-			if (!_httpHelper.TryCallApi(url, _configuration.GetAuthToken(), out var apiResponse))			
+			if (!_httpHelper.TryCallApi(url, _configuration.GetAuthToken(), out var apiResponse, _timeout, _linux))			
 			{
 				return new List<PlasticTask>();
 			}
@@ -75,13 +80,14 @@ namespace Equilaterus.GitHubExtension.Provider
 		{
 			// Call API
 			var url = _configuration.GetApiUrlForTaskCommenting(taskId);
-			return _httpHelper.TryCallApi(url, _configuration.GetAuthToken(), out _, "POST", $"{{\"body\": \"{ message }\"}}");
+			var body = $"{{\"body\": \"{ message }\"}}";
+			return _httpHelper.TryCallApi(url, _configuration.GetAuthToken(), out _, _timeout, _linux, "POST", body);
 		}
 
 		public void TestConnection(IssueTrackerConfiguration byPassConfiguration)
 		{
 			var url = byPassConfiguration.GetApiUrlForTasks(null);
-			_httpHelper.CallApi(url, byPassConfiguration.GetAuthToken());
+			_httpHelper.CallApi(url, byPassConfiguration.GetAuthToken(), _timeout, _linux);
 		}
 
 		private void LogException(Exception e)
